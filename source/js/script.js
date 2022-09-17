147e36b6c34bea7284094b649896372b9b191295
Roman Boronin <r.vboronin@gmail.com>
2022-09-17 08:47:40 +0300

    удаляет кнопку  меню гамбургер  в мобильной версии при отключенном js

--- source/js/script.js
+++ source/js/script.js
@@ -2,9 +2,13 @@ const pageHeaderBtn = document.querySelector('.page-header__btn');
 const pageHeaderItemBurger = document.querySelector('.page-header__item-burger');
 const navigation = document.querySelector('.nav');
 
+navigation.classList.remove('nav--nojs');
+pageHeaderBtn.classList.remove('page-header__btn--hidden');
 
 pageHeaderBtn.onclick = function () {
-  navigation.classList.toggle('nav--active');
-  pageHeaderBtn.classList.toggle('page-header__btn-close');
-  pageHeaderItemBurger.classList.toggle('page-header__item-close');
+navigation.classList.toggle('nav--active');
+pageHeaderBtn.classList.toggle('page-header__btn-close');
+pageHeaderItemBurger.classList.toggle('page-header__item-close');
 };
+
+
