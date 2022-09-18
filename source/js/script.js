d7de6c8ff3f268db03b980a5d10a9744191e339b
Roman Boronin <r.vboronin@gmail.com>
2022-09-18 20:45:47 +0300

    добавляет стили для страницы каталог версии desktop

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,8 @@
 const pageHeaderBtn = document.querySelector('.page-header__btn');
 const pageHeaderItemBurger = document.querySelector('.page-header__item-burger');
 const navigation = document.querySelector('.nav');
+const map = document.querySelector('.map__img');
+const pin = document.querySelector('.map__pin');
 
 navigation.classList.remove('nav--nojs');
 pageHeaderBtn.classList.remove('page-header__btn--hidden');
